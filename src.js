d7ec665a7b5af96280c49516f3f8a3c6821acc88
milkon <melkon_2010@hotmail.com>
2021-03-30 13:55:15 +0200

    added onclick to li tags and cleaning input field onfocus

--- src.js
+++ src.js
@@ -2,28 +2,16 @@ const doFunction = function () {
     let userinput = document.getElementById('todo');
     const ul = document.getElementById('List');
     let li = document.createElement('li');
-
+    li.addEventListener('click',eventHandler);
     li.innerHTML = userinput.value;
-    ul.appendChild(li);  
+    ul.appendChild(li);
 }
-// document.getElementById('todo').addEventListener('todo',doFunction);
-
-let Button = document.getElementById("todo");
-Button.addEventListener("Click", function(){
-    console.log("Milkon");
-});
 
 
-
-
-
-
-
-// let input = document.getElementById("todo");
-// if(input){
-//     input.addEventListener('keyup', function(event) {
-//     if (event.keyCode == 13) {
-//         doFunction;
-//     }
-//   });
-//   }
+function eventHandler(event){
+    if(event.type == 'click'){
+        event.target.className = 'checked';
+    }else{
+        console.log("error!");
+    }
+}
