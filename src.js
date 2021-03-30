2bbe72c55924064c77f997648dd23acb88ccb87d
milkon <melkon_2010@hotmail.com>
2021-03-30 18:36:24 +0200

    saving todo tasks to the browser local storage so they are reloaded when refreshing the page

--- src.js
+++ src.js
@@ -1,21 +1,53 @@
+let myStorage = window.localStorage;
+
+window.onload = function(){
+    readLocalStorage();
+}
+const readLocalStorage =  function(){
+   
+    for(let i = 0;i<myStorage.length;i++){
+        let val = myStorage.getItem(myStorage.key(i));
+        let li = createElement();
+        li.innerHTML = val;
+        li.appendChild(createButton());
+        document.getElementById('List').appendChild(li);
+    }
+}
 const doFunction = function () {
-    let li = document.createElement('li');
-    li.addEventListener('click',eventHandler);
+    let uniqValue = Date.now();
+    myStorage.setItem(uniqValue, document.getElementById('todo').value);
+    let li = createElement();
     li.innerHTML = document.getElementById('todo').value;
     li.appendChild(createButton());
     document.getElementById('List').appendChild(li);
 }
 
+const createElement = function(){
+    let li = document.createElement('li');
+    li.addEventListener('click',eventHandler);
+   
+    return li;
+}
+
 const createButton = function(){
     const button = document.createElement('BUTTON');
-    button.className = 'x_button';
+    button.className = 'deleteButton';
     button.addEventListener('click',function(){
        button.parentElement.remove();
        button.remove();
+    //    localStorageRemoveItem(button);
     })
     button.innerHTML = 'Delete';
     return button
 }
+
+// const localStorageRemoveItem = function(button){
+//     for(let i = 0;i<myStorage.length;i++){
+//         if(button.parentElement.value == myStorage.getItem(myStorage.key(i))){
+//             myStorage.removeItem(localStorage.key(i));
+//         }
+//     }
+// }
 
 function eventHandler(event){
     if(event.type == 'click'){
