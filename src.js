bb2ed7aded9502555be957aa0a8780e4f692e416
milkon <melkon_2010@hotmail.com>
2021-03-30 12:35:49 +0200

    fixed bugs and added li.checked klass to check the todo list elements when user is done with tasks

--- src.js
+++ src.js
@@ -2,17 +2,28 @@ const doFunction = function () {
     let userinput = document.getElementById('todo');
     const ul = document.getElementById('List');
     let li = document.createElement('li');
-    
+
     li.innerHTML = userinput.value;
     ul.appendChild(li);  
 }
-document.getElementById('todo').onchange = doFunction;
+// document.getElementById('todo').addEventListener('todo',doFunction);
 
-let input = document.getElementById("todo");
-if(input){
-    input.addEventListener('keyup', function(event) {
-    if (event.keyCode == 13) {
-        doFunction;
-    }
-  });
-  }
+let Button = document.getElementById("todo");
+Button.addEventListener("Click", function(){
+    console.log("Milkon");
+});
+
+
+
+
+
+
+
+// let input = document.getElementById("todo");
+// if(input){
+//     input.addEventListener('keyup', function(event) {
+//     if (event.keyCode == 13) {
+//         doFunction;
+//     }
+//   });
+//   }
